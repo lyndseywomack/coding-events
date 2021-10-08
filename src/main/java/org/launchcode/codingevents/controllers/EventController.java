3e8afb4fd67b2be2f399fade09d2de9903adac29
Lyndsey Womack <lyndseywomack@lyndseys-air.lan>
2021-10-08 17:33:02 -0500

    trying again

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,23 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class EventController {
+
+    @GetMapping
+    public String displayAllEvents(Model model){
+        List<String> events = new ArrayList<>();
+        events.add("Code with Pride");
+        events.add("Strange Loop");
+        events.add("Apple WWCD");
+        events.add("SpringOne Platform");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+}
